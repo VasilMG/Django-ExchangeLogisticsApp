c52a6e1ed4d7f1bda30ff1a2a021b85ec280bb25
VasilMG <119819354+VasilMG@users.noreply.github.com>
2023-03-11 22:04:51 +0200

    deleting old files

--- ExchangeLogistics/exchange/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from ExchangeLogistics.exchange.models import Offer
-
-
-@admin.register(Offer)
-class OfferAdmin(admin.ModelAdmin):
-    list_display = ['created_on', 'company']
