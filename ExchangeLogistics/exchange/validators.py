78c73a4b1ad3e5d25f37b66b8fc5b38db55e9564
VasilMG <119819354+VasilMG@users.noreply.github.com>
2023-03-17 21:07:55 +0200

    Update validators.py

--- ExchangeLogistics/exchange/validators.py
+++ ExchangeLogistics/exchange/validators.py
@@ -4,14 +4,17 @@ import re
 def date_validator(value):
     if value < datetime.date.today():
         raise ValueError('Date cannot be in the past.')
+    return value
 
 def validate_load_size(value):
     if 0 > value or value > 15:
         raise ValueError('The value should be between 0 and 15.00 meters')
+    return value
 
 def validate_load_weight(value):
     if 0 > value or value > 28:
         raise ValueError('The value should be between 0 and 28 tons.')
+    return value
 
 def phone_number_validator(value):
     pattern = r'(\+\d{9,15}$)'
