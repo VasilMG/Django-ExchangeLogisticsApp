ca0273906c68fd3c5e2d743a03c316b839010311
VasilMG <119819354+VasilMG@users.noreply.github.com>
2023-07-05 03:08:16 +0300

    Update settings.py

--- ExchangeLogistics/settings.py
+++ ExchangeLogistics/settings.py
@@ -15,7 +15,10 @@ SECRET_KEY = 'purple-unicorn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
