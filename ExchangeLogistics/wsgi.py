0112f720aa630941f6b6b9da87798e2a15ec75ee
VasilMG <vassil.manolov@abv.bg>
2023-07-19 15:53:55 +0300

    add production.py

--- ExchangeLogistics/wsgi.py
+++ ExchangeLogistics/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ExchangeLogistics.settings')
+# If WEBSITE_HOSTNAME is defined as an environment variable, then we're running
+# on Azure App Service and should use the production settings in production.py.
+settings_module = "azuresite.production" if 'WEBSITE_HOSTNAME' in os.environ else 'azuresite.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
