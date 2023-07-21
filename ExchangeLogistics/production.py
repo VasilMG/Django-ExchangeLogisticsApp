0835103b6977a9b32be342a41cc715e4e708b7d5
VasilMG <vassil.manolov@abv.bg>
2023-07-21 10:31:27 +0300

    add csrf and change wsgi

--- ExchangeLogistics/production.py
+++ ExchangeLogistics/production.py
@@ -5,6 +5,8 @@ import os
 # that Azure automatically creates for us.
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
+CSRF_TRUSTED_ORIGINS = ['WEBSITE_HOSTNAME']
+
 # WhiteNoise configuration
 MIDDLEWARE = [                                                                   
     'django.middleware.security.SecurityMiddleware',
--- ExchangeLogistics/wsgi.py
+++ ExchangeLogistics/wsgi.py
@@ -13,8 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 # If WEBSITE_HOSTNAME is defined as an environment variable, then we're running
 # on Azure App Service and should use the production settings in production.py.
-settings_module = "ExchangeLogistics.production" if 'WEBSITE_HOSTNAME' in os.environ else 'ExchangeLogistics.settings'
+# settings_module = "ExchangeLogistics.production" if 'WEBSITE_HOSTNAME' in os.environ else 'ExchangeLogistics.settings'
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', "ExchangeLogistics.production")
 
 application = get_wsgi_application()
