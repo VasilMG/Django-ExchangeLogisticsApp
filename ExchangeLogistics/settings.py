d004ee55cc1b3035d8d83c6e053bdc432fe5c104
VasilMG <119819354+VasilMG@users.noreply.github.com>
2023-07-31 23:50:53 +0300

    responsive changes and middleware

--- ExchangeLogistics/settings.py
+++ ExchangeLogistics/settings.py
@@ -44,6 +44,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'ExchangeLogistics.common.middleware.NoDjangoAdminForEndUserMiddleware',
 ]
 
 ROOT_URLCONF = 'ExchangeLogistics.urls'
