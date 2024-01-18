36c30f7404ff816c98805543fa62f9f98f34cfa5
VasilMG <119819354+VasilMG@users.noreply.github.com>
2024-01-19 01:20:46 +0200

    add to allowed hosts

--- ExchangeLogistics/settings.py
+++ ExchangeLogistics/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'purple-unicorn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'vasil-logistics.azurewebsites.net']
 
 
 # Application definition
